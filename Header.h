ef7b3d95c75e321cdd41ddb3d3ec27e28b535da4
Quied <82113495+Quied@users.noreply.github.com>
2021-12-18 15:57:58 +0200

    Add files via upload

--- Header.h
+++ Header.h
@@ -12,11 +12,12 @@ public:
 
 	Hyb() {}
 	Hyb(D _Data) : Data(_Data), Next(nullptr), FIRST(nullptr), LAST(nullptr) {}
-	~Hyb() { delete Next; }
-
+	
 
 	friend std::ostream& operator<< (std::ostream& out, const Hyb & node) {
-		out << "Data: " << node.Data;
+		out << "Begin: " << _TYPEINFO_(node).FIRST->Data; std::cout << std::endl;
+		out << "Last: " << _TYPEINFO_(node).LAST->Data; std::cout << std::endl;
+		out << "Type: " << typeid(node).name();
 		return out;
 	}
 
@@ -25,7 +26,7 @@ public:
 	Hyb* FIRST = nullptr;
 	Hyb* LAST = nullptr;
 
-	void add(Hyb node, const double data) {
+	void add(Hyb node, const D data) {
 		Hyb* el = new Hyb(data);
 		if (FIRST == NULL) {
 			FIRST = el;
@@ -35,7 +36,7 @@ public:
 		LAST = el;}
 	}
 
-	void sort(Hyb **node) {
+	void sort(Hyb *node) {
 		assert(typeid(FIRST->Data) == typeid(int));
 		for (int j = 0; j < size((*node)); j++) {
 			(*node) = FIRST;
@@ -56,11 +57,18 @@ public:
 	    OUT = OUT->Next;
 		}	std::cout << std::endl; }
 
-	void push_back(Hyb* node, const double data) { 
-	
+	void push_back(Hyb node, const D data) { 
+		Hyb* el = new Hyb(data);
+		if (LAST != nullptr) { LAST->Next = el; LAST = el; }
+		else { LAST = el; FIRST = el; }
 	}
 
-	void push_front() { }
+	void push_front(Hyb node, const D data) { 
+		Hyb* el = new Hyb(data);
+		if (FIRST != nullptr) { el->Next = FIRST; FIRST = el; }
+		else { FIRST = el; LAST = el; }
+	}
+
 	void implace() { }
 	void insert() { }
 
--- Source.cpp
+++ Source.cpp
@@ -5,16 +5,20 @@
 
 int main() {
 
-	Hyb<int> Hybrid;
-	Hybrid.add(Hybrid, 17);
+	Hyb<double> Hybrid;
+	Hybrid.add(Hybrid, 7777);
 	Hybrid.add(Hybrid, 16);
 	Hybrid.add(Hybrid, 18);
 	Hybrid.add(Hybrid, 17);
-	
+	Hybrid.push_back(Hybrid, 999);
+	Hybrid.push_front(Hybrid, 777);
+	Hybrid.push_back(Hybrid, 17);
+	Hybrid.push_front(Hybrid, 17);
+
 	Hybrid.print();
 
-	std::cout << _TYPEINFO_(Hybrid).Data << std::endl;
-	std::cout << typeid(Hybrid).name() << std::endl;
+	std::cout << Hybrid; std::cout << std::endl;
+
 	
 
 }
